d08b181957ae9229b11cebf4351621b3f4574297
Henrik Spolander <hspolander@gmail.com>
2022-08-22 13:26:04 +0200

    Walk through jokes app tutorial

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,53 +1,47 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
-
-const prisma = new PrismaClient();
+const db = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
-
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
-
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  console.log(`Database has been seeded. 🌱`);
+  await Promise.all(
+    getJokes().map((joke) => {
+      return db.joke.create({ data: joke });
+    })
+  );
 }
 
-seed()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+seed();
+
+function getJokes() {
+  // shout-out to https://icanhazdadjoke.com/
+
+  return [
+    {
+      name: "Road worker",
+      content: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`,
+    },
+    {
+      name: "Frisbee",
+      content: `I was wondering why the frisbee was getting bigger, then it hit me.`,
+    },
+    {
+      name: "Trees",
+      content: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`,
+    },
+    {
+      name: "Skeletons",
+      content: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`,
+    },
+    {
+      name: "Hippos",
+      content: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`,
+    },
+    {
+      name: "Dinner",
+      content: `What did one plate say to the other plate? Dinner is on me!`,
+    },
+    {
+      name: "Elevator",
+      content: `My first time using an elevator was an uplifting experience. The second time let me down.`,
+    },
+  ];
+}
